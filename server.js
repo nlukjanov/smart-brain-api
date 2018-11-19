73e67dd6481a1c8aa8c5ca8c4f1f895256ec5d68
Nikita Lukjanov <nlukjanov@gmail.com>
2018-11-19 06:54:43 +0000

    refactored signin function to be able issue and accept token (next task)

--- server.js
+++ server.js
@@ -24,7 +24,7 @@ app.use(cors())
 app.use(bodyParser.json());
 
 app.get('/', (req, res)=> { res.send("hello! it's working! again, but not db") })
-app.post('/signin', signin.handleSignin(db, bcrypt))
+app.post('/signin', signin.signinAuthentication(db, bcrypt))
 app.post('/register', (req, res) => { register.handleRegister(req, res, db, bcrypt) })
 app.get('/profile/:id', (req, res) => { profile.handleProfileGet(req, res, db)})
 app.post('/profile/:id', (req, res) => {profile.handleProfileUpdate(req, res, db)})
