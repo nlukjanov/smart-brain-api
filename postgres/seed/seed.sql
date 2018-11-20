66536bd4c444b31a455e4699a517ce30d59dac41
Nikita Lukjanov <nlukjanov@gmail.com>
2018-11-20 07:58:16 +0000

    added pet and age to db

--- postgres/seed/seed.sql
+++ postgres/seed/seed.sql
@@ -1,6 +1,6 @@
 BEGIN TRANSACTION;
 
-INSERT into users (name, email, entries, joined ) values ('Jessie', 'jessie@gmail.com', 5, '2018-01-01');
+INSERT into users (name, email, age, pet, entries, joined ) values ('Jessie', 'jessie@gmail.com', 25, 'dragon', 5, '2018-01-01');
 INSERT into login (hash, email) values ('$2a$10$bLru3.ZS44UvbCf1C0WA..kSGIuGZj4sKA/oxHPOIFxHzAk1bZn5G', 'jessie@gmail.com');
 
 COMMIT;
--- postgres/tables/users.sql
+++ postgres/tables/users.sql
@@ -4,6 +4,8 @@ CREATE TABLE users (
   id serial PRIMARY KEY,
   name VARCHAR(100),
   email text UNIQUE NOT NULL,
+  age SMALLINT DEFAULT 0,
+  pet VARCHAR(100),
   entries BIGINT DEFAULT 0,
   joined TIMESTAMP NOT NULL
 );
